86a3f76a22a04ebb5558bc3f53cc28307e0b0d55
xuedongliu <390128150@qq.com>
2019-03-12 17:49:31 +0800

    完成select大部分功能。

--- component/select/select.js
+++ component/select/select.js
@@ -5,49 +5,113 @@
  * @returns {HTMLSelectElement}
  */
 HTMLSelectElement.prototype.f = function (data) {
-  // console.log(12345678);
   const $this = this;
+  const d = data || {};
   const parentElement = $this.parentElement;
-  let value = $this.value || 0;
-  $this.v = {};
+  let value = d.selectValue || $this.value || 0;
+  $this.v = {
+    show: false
+  };
   let select = create('span', parentElement);
   let dropdown = create('ul', parentElement);
+  let dropdownListTemp = $this.querySelectorAll('option');
+  let dropdownList = [];
+  if (d.list) {
+    dropdownList = [].concat(d.list);
+
+  } else {
+    for (let i = 0; i < dropdownListTemp.length; i++) {
+      const item = dropdownListTemp[i];
+      dropdownList.push({
+        val: item.value,
+        innerHTML: item.innerHTML
+      });
+    }
+  }
   select.classList.add('chosen-value');
   dropdown.classList.add('selectList');
-  select.innerHTML = $$('option[value="' + value + '"]', $this).textContent;
-  for (let i = 0; i < $this.children.length; i++) {
-    const item = $this.children[i];
+  for (let i = 0; i < dropdownList.length; i++) {
+    const item = dropdownList[i];
     let li = create('li', dropdown);
-    li.value = item.value;
-    li.innerHTML = item.textContent;
+    for (let key in item) {
+      if (item.hasOwnProperty(key)) {
+        li[key] = item[key];
+      }
+    }
+    if (item.val === value) {
+      $this.v.select = li;
+      select.innerHTML = item.innerHTML;
+    }
     li.addEventListener('click', function (e) {
       e.stopPropagation();
       $this.v.select = this;
-      dropdown.classList.remove('open');
+      $this.v.show = false;
     });
   }
-  $this.v.select = $$('li[value="' + value + '"]',dropdown);
   $this.v.select.classList.add('open');
   $this.style.display = 'none';
 
   select.addEventListener('click', function (e) {
     e.stopPropagation();
-    this.classList.toggle('open');
-    dropdown.classList.toggle('open');
+    // this.classList.toggle('open');
+    $this.v.show = !$this.v.show;
   });
   observe($this.v, function (key, val, newVal) {
-    select.innerHTML = newVal.innerHTML;
-    val.classList.remove('open');
-    newVal.classList.add('open');
-    $this.value = newVal.value;
-    $this.dispatchEvent(new Event('change'));
+    switch (key) {
+      case 'select':
+        select.innerHTML = newVal.innerHTML;
+        val.classList.remove('open');
+        newVal.classList.add('open');
+        $this.value = newVal.val;
+        $this.dispatchEvent(new Event('change'));
+        break;
+      case 'show':
+        if (newVal) {
+          dropdown.classList.add('open');
+          select.classList.add('open');
+        } else {
+          dropdown.classList.remove('open');
+          select.classList.remove('open');
+        }
+        break;
+      default:
+        break;
+    }
 
   });
+  window.addEventListener('click', function (e) {
+    e.stopPropagation();
+    // console.log(e.target===parentElement||parentElement.contains(e.target));
+    // if (e.target)
+    if (e.target !== parentElement && !parentElement.contains(e.target)) {
+      $this.v.show = false;
+    }
+  }, false);
   return $this;
 };
 
-let select = document.querySelector('select.select').f();
-select.addEventListener('change',function () {
+
+let data = {
+  selectValue: 'D01',
+  list: [
+    {
+      val: 'D01',
+      innerHTML: 'test11'
+    },
+    {
+      val: 'D02',
+      innerHTML: 'test21'
+    },
+    {
+      val: 'D03',
+      innerHTML: 'test31'
+    }
+  ]
+};
+let select = document.querySelectorAll('select.select');
+select[0].f();
+select[1].f(data);
+select[1].addEventListener('change', function () {
   console.log(this.value);
 });
 
@@ -57,7 +121,15 @@ select.addEventListener('change',function () {
 *   list: [
 *     {
 *       value: 'D01',
-*       html: 'test'
+*       html: 'test1'
+*     },
+*     {
+*       value: 'D02',
+*       html: 'test2'
+*     },
+*     {
+*       value: 'D03',
+*       html: 'test3'
 *     }
 *   ]
 * }
